1549c890c3ce6d779de5564d03f5545118b4167f
Suraj-Shaikh <shaikhsuraj8219@gmail.com>
2024-09-01 20:04:31 +0530

    add clip to selection

--- main.js
+++ main.js
@@ -22,20 +22,21 @@ function GetTalukaName() {
   selectVillage.innerHTML = "";
   let uniVillages = [];
   for (let i = 0; i < villageData.features.length; i++) {
-    let dist = villageData.features[i].properties.dtmname;
-    let thn = villageData.features[i].properties.thmname;
-    let vill = villageData.features[i].properties.vilmname;
-    if (thn == selectCityText) {
+    let dist = villageData.features[i].properties;
+    let thn = villageData.features[i].properties;
+    let vill = villageData.features[i].properties;
+    if (thn.thmname == selectCityText) {
       let chk = uniVillages.includes(vill);
       if (chk == false) {
-        uniVillages.push(vill);
+        uniVillages.push(vill.vincode);
         var option = document.createElement("option");
-        option.text = vill;
-        option.value = vill;
+        option.text = vill.vilmname;
+        option.value = vill.vincode;
         selectVillage.appendChild(option);
       }
     }
   }
+  LoadTaluka()
 }
 
 function GetDistrictName() {
@@ -46,16 +47,15 @@ function GetDistrictName() {
   selectTaluka.innerHTML = "";
 
   for (let i = 0; i < villageData.features.length; i++) {
-    let dist = villageData.features[i].properties.dtmname;
-    let thn = villageData.features[i].properties.thmname;
-    // console.log(thn)
-    if (dist == selectDistrictText) {
-      let chk = uniTaluka.includes(thn);
+    let dist = villageData.features[i].properties;
+    let thn = villageData.features[i].properties;
+    if (dist.dtmname == selectDistrictText) {
+      let chk = uniTaluka.includes(thn.thncode);
       if (chk == false) {
-        uniTaluka.push(thn);
+        uniTaluka.push(thn.thncode);
         var option = document.createElement("option");
-        option.text = thn;
-        option.value = thn;
+        option.text = thn.thmname;
+        option.value = thn.thncode;
         selectTaluka.appendChild(option);
       }
     }
@@ -72,14 +72,14 @@ let uniDistrict = [];
 for (let i = 0; i < villageData.features.length; i++) {
   let dist = villageData.features[i].properties;
 
-  let chk = uniDistrict.includes(dist.dtmname);
+  let chk = uniDistrict.includes(dist.dtncode);
 
 
   if (chk == false) {
-    uniDistrict.push(dist.dtmname);
+    uniDistrict.push(dist.dtncode);
     var option = document.createElement("option");
     option.text = dist.dtmname;
-    option.value = dist.dtmname;
+    option.value = dist.dtncode;
     selectdistrict.appendChild(option);
     // seletState.add(new Option(stateName, stateName))
   }
@@ -103,12 +103,62 @@ function LoadDistrict() {
     }
   });
 
-  let newGeoJson = { type: 'FeatureCollection', features: filterFeatures}
+  let distGeoJson = { type: 'FeatureCollection', features: filterFeatures}
 
   map.removeLayer(myLayer)
-  myLayer = L.geoJSON(newGeoJson).addTo(map);
+  myLayer = L.geoJSON(distGeoJson).addTo(map);
   map.fitBounds(myLayer.getBounds());
 }
 
+function LoadTaluka() {
+  let districtData = villageData.features;
+  filterFeatures = [];
+
+  let select1 = document.getElementById("thnid");
+  let selectTalukaText = select1.options[select1.selectedIndex].value;
+
+  let select = document.getElementById("distid");
+  let selectDistrictText = select.options[select.selectedIndex].value;
+
+  districtData.forEach(function (e) {
+    let disName = e.properties['thncode'];
+    if (disName == selectTalukaText) {
+      filterFeatures.push(e);
+    }
+  });
+
+  let talukaGeoJson = { type: 'FeatureCollection', features: filterFeatures}
+
+  map.removeLayer(myLayer)
+  myLayer = L.geoJSON(talukaGeoJson).addTo(map);
+  map.fitBounds(myLayer.getBounds());
+}
+
+function LoadVillage() {
+  let districtData = villageData.features;
+  filterFeatures = [];
+
+  let select2 = document.getElementById("villid");
+  let selectVillageText = select2.options[select2.selectedIndex].value;
+
+  let select1 = document.getElementById("thnid");
+  let selectTalukaText = select1.options[select1.selectedIndex].value;
+
+  let select = document.getElementById("distid");
+  let selectDistrictText = select.options[select.selectedIndex].value;
+
+  districtData.forEach(function (e) {
+    let disName = e.properties['vincode'];
+    if (disName == selectVillageText) {
+      filterFeatures.push(e);
+    }
+  });
+
+  let villageGeoJson = { type: 'FeatureCollection', features: filterFeatures}
+
+  map.removeLayer(myLayer)
+  myLayer = L.geoJSON(villageGeoJson).addTo(map);
+  map.fitBounds(myLayer.getBounds());
+}
 
 
